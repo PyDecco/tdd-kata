74f3561061d98c29b90debf4c967348231123ff1
Andre Urpia <dreurpia@gmail.com>
2021-03-01 22:45:16 -0300

    Greeter()  shoud be able trim function effect  not Pass

--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -3,3 +3,8 @@ test('shoud be able return Hello with name from greeter ', () => {
   const greeter = new Greeter()
   expect(greeter.greet('Deco')).toEqual('Hello Deco');
 });
+test('shoud be able trim function effect ', () => {
+  const greeter = new Greeter()
+  expect(greeter.greet('Deco       ')).toEqual('Hello Deco');
+  expect(greeter.greet('Joe       ')).toEqual('Hello Joe');
+});
