e5e813622d3cddc455919392393d673cddb7e0bf
Andre Urpia <dreurpia@gmail.com>
2021-03-18 00:21:50 -0300

    🟢 should be able first letter uppercase

--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -1,17 +1,15 @@
 const Greeter = require('./Greeter');
 
 describe('Greeter', () => {
-test('shoud be able return Hello with name from greeter ', () => {
   const greeter = new Greeter()
+test('shoud be able return Hello with name from greeter ', () => {
   expect(greeter.greet('Deco')).toEqual('Hello Deco');
 });
 test('shoud be able trim function effect ', () => {
-  const greeter = new Greeter()
   expect(greeter.greet('Deco       ')).toEqual('Hello Deco');
   expect(greeter.greet('             Joe ')).toEqual('Hello Joe');
 });
 test('should be able first letter uppercase ', () => {
-  const greeter = new Greeter()
   expect(greeter.greet('deco')).toEqual(('Hello Deco'))
 })
 })
