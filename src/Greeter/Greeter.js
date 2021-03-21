fd7f81464c5ebddffeeabbf9c74c34bc8309eb92
Andre Urpia <dreurpia@gmail.com>
2021-03-20 23:05:12 -0300

    🟢 should be able greet according to the hours 6:00 - 12:00

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -2,9 +2,22 @@ class Greeter {
   constructor() {
   }
   greet(name) {
+    const currentTime = new Date().getHours()
+    
     const nameFormatted = name.charAt(0).toUpperCase() + name.slice(1);
     const finalName = nameFormatted.trim();
-    return 'Hello '+ finalName;
+    const greeting = this.searchHour(currentTime);
+    
+    return greeting + finalName;
+  }
+
+  searchHour(currentTime){
+    let greeting;     
+    if (6 < currentTime && currentTime < 12 )
+      greeting = 'good morning ';
+      else
+      greeting = 'Hello ';
+    return greeting
   }
 }
 module.exports = Greeter;
--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -1,18 +1,34 @@
 const Greeter = require('./Greeter');
 
 describe('Greeter', () => {
-  const greeter = new Greeter()
+  let greeter;
+  let date;
+  beforeEach(() => {
+    greeter = new Greeter
+    
+  });
+
 test('shoud be able return Hello with name from greeter ', () => {
   expect(greeter.greet('Deco')).toEqual('Hello Deco');
 });
+
 test('shoud be able trim function effect ', () => {
   expect(greeter.greet('Deco       ')).toEqual('Hello Deco');
   expect(greeter.greet('             Joe ')).toEqual('Hello Joe');
 });
+
 test('should be able first letter uppercase ', () => {
   expect(greeter.greet('deco')).toEqual(('Hello Deco'));
 });
+
 test('should be able greet according to the hours 6:00 - 12:00', () => {
-  expect(greeter.greet('deco')).toEqual(('good morning Deco'))
+  jest.spyOn(global, "Date").mockImplementation(() => {
+    return {
+      getHours() {
+        return 10;
+      },
+    };
+  });
+  expect(greeter.greet('deco')).toEqual(('good morning Deco'));
 });
-})
+});
