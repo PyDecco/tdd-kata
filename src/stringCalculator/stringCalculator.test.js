92511f61ec1244956a905aa0eadda31afd9bed32
Andre Urpia <dreurpia@gmail.com>
2021-03-27 18:54:22 -0300

    🔴 shoud be able return zero for empty string

--- src/stringCalculator/stringCalculator.test.js
+++ src/stringCalculator/stringCalculator.test.js
@@ -1,9 +1,9 @@
-//const stringCalculator = require("./stringCalculator");
+const stringCalculator = require("./stringCalculator");
 
-//describe("stringCalculator", () => {
- // beforeEach(() => {});
-//  test("shoud be able return zero for empty string", () => {
-//    const result = stringCalculator("")
-//    expect(result).toEqual(0);
-//  });
-//});
+describe("stringCalculator", () => {
+  beforeEach(() => {});
+  test("shoud be able return zero for empty string", () => {
+    const result = stringCalculator("");
+    expect(result).toEqual(0);
+  });
+});
