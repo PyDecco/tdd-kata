78fcbaced2d2ccf06392f632d0beb25b29e54f02
Andre Urpia <dreurpia@gmail.com>
2021-03-28 18:06:16 -0300

    🟢 shoud be able return error message for negative value

--- src/stringCalculator/stringCalculator.js
+++ src/stringCalculator/stringCalculator.js
@@ -1,11 +1,15 @@
 module.exports = (value) => {
   let result = 0;
+  let negativeElement = []; 
   if (value.match(",") || value.match("\n")) {
     var separators = ["\\,", "\n"];
     const arrayElements = value.split(new RegExp(separators.join("|"), "g"));
-    
     for (let i = 0; i < arrayElements.length; i++) {
       result += parseInt(arrayElements[i]);
+      if(parseInt(arrayElements[i]) < 0 ) negativeElement.push(arrayElements[i])
+    }
+    if(negativeElement.length){
+      return `negativos não permitidos: ${negativeElement}`
     }
     return result
   }
