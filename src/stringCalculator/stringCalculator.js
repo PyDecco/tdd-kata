f0611851030da24693d6fc288df3d48411e2a75c
Andre Urpia <dreurpia@gmail.com>
2021-03-28 17:53:00 -0300

    🟢 shoud be able return a sum for new line and comma between numbers

--- src/stringCalculator/stringCalculator.js
+++ src/stringCalculator/stringCalculator.js
@@ -1,8 +1,14 @@
 module.exports = (value) => {
-  let result;
-  if (value.charAt(1) == "," || value.charAt(1) == "\n") {
-    return result = parseInt(value.charAt(0)) + parseInt(value.charAt(2))
+  let result = 0;
+  if (value.match(",") || value.match("\n")) {
+    var separators = ["\\,", "\n"];
+    const arrayElements = value.split(new RegExp(separators.join("|"), "g"));
+    
+    for (let i = 0; i < arrayElements.length; i++) {
+      result += parseInt(arrayElements[i]);
+    }
+    return result
   }
-  result = value ? value : 0
-  return parseInt(result)
-}
+  result = value ? value : 0;
+  return parseInt(result);
+};
